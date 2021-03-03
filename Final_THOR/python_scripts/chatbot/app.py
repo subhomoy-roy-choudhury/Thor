a37a106b3f0aa1fa699322913042ba4aad563c6f
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2021-03-03 18:19:05 +0530

    login page in chatbot

--- Final_THOR/python_scripts/chatbot/app.py
+++ Final_THOR/python_scripts/chatbot/app.py
@@ -1,9 +1,13 @@
-from flask import Flask,render_template, jsonify, request
+from flask import Flask,render_template, jsonify, request,redirect, url_for
 from flask_cors import CORS
 import chat_API
 import pywhatkit
+import pickle
+from pyngrok import ngrok
+
 app = Flask(__name__)
 CORS(app)
+database={'subho':'hero','soham':'chomu','yash':'pagla'}
 
 # @app.route('/predict',methods=['POST'])
 # def index():
@@ -12,18 +16,40 @@ CORS(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("login.html")
+
+@app.route('/form_login',methods=['POST','GET'])
+def login():
+    name1=request.form['username']
+    pwd=request.form['password']
+    if name1 not in database:
+	    return render_template('login.html',info='Invalid User')
+    else:
+        if database[name1]!=pwd:
+            return render_template('login.html',info='Invalid Password')
+        else:
+	        return render_template('index.html',name=name1)
+
+
 
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('user_input')
+    userText = userText.lower()
     if "search" in userText:
         userText = userText.replace('search', '')  
         pywhatkit.search(userText) 
         return "searched"+str(userText)
     return str(chat_API.brain(userText))
 
+@app.route('/logout')
+def logout():
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=5000, debug=True)
+    # url = ngrok.connect(5000).public_url
+    # print(' * Tunnel URL:', url)
+    # ngrok.kill()
+    app.run(host='127.0.0.1', port=5000, debug=True)
+    
