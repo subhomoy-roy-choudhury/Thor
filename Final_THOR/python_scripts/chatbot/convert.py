dff35c23503b392158090c71f01313bfee80ea86
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2021-03-11 18:59:50 +0530

    yaml convertion

--- Final_THOR/python_scripts/chatbot/convert.py
+++ Final_THOR/python_scripts/chatbot/convert.py
@@ -1,6 +1,6 @@
 import json
 
-with open('sample1.json', 'r') as f:
+with open('jsons/json_normal/conversations.yml.json', 'r') as f:
     intents = json.load(f)
 
 
@@ -16,22 +16,23 @@ for key,value in intents.items():
             pattern_list = []
             responses_list = []
             tag = str(str(intents['categories'][0])) +" "+str(i)
-            patterns = (intents['conversations'][i][0])
-            responses = (intents['conversations'][i][1])
+            patterns = (intents['conversations'][i][:1])
+            responses = (intents['conversations'][i][1:])
             print(patterns,tag,responses,i)
             pattern_list.append(patterns)
             responses_list.append(responses)
-            dict2 = {"tag":tag,"patterns":pattern_list,"responses":responses_list}
+            dict2 = {"tag":tag,"patterns":patterns,"responses":responses}
             list1.append(dict2)
             print(list1)
     if key == 'categories':
-        #print(key)
-        key = str(intents['categories'][0])
-        
-        dict1.__setitem__(str(key),list1)
-        #print("%%%%%%"+ str(key))
 
+        key = str(intents['categories'][0])
+        a='jsons/json_convert/'+str(key) + '.json'
+        # dict1.__setitem__(str(key),list1)
+        dict1.__setitem__("intents",list1)
 print(intents.keys())
 
-with open('sample_output.json', 'w') as outfile:
-    json.dump(dict1, outfile,ensure_ascii=False, indent=2)
+
+
+with open(a, 'w') as outfile:
+    json.dump(dict1, outfile,ensure_ascii=False, indent=1)
