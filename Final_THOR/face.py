eac187414bd1c82f183f24978de7c7630cc844aa
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2021-03-14 11:37:41 +0530

    NEW Script for face Recognition

--- Final_THOR/face.py
+++ Final_THOR/face.py
@@ -3,76 +3,58 @@
 @author: Subhomoy.Roy.Choudhury
 """
 
-# Face Recognition
-
-# Importing the libraries
-from PIL import Image
-from keras.applications.vgg16 import preprocess_input
-import base64
-from io import BytesIO
-import json
-import random
 import cv2
-from keras.models import load_model
 import numpy as np
+import face_recognition
+import os
+from datetime import datetime
 
-from keras.preprocessing import image
+# from PIL import ImageGrab
 
-model = load_model('python_scripts/Facial_Recognition/facefeatures_new_model.h5')
+path = 'python_scripts/Facial_Recognition/Images_train'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
 
-# Loading the cascades
-face_cascade = cv2.CascadeClassifier('python_scripts/Facial_Recognition/haarcascade_frontalface_default.xml')
 
-def face_extractor(img):
-    # Function detects faces and returns the cropped face
-    # If no face detected, it returns the input image
-    
-    #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(img, 1.3, 5)
-    
-    if faces ==():
-        return None
-    
-    # Crop all faces found
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),2)
-        cropped_face = img[y:y+h, x:x+w]
+def findEncodings(images):
+    encodeList = []
 
-    return cropped_face
 
-# Doing some Face Recognition with the webcam
-video_capture = cv2.VideoCapture(0)
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
 def face():
-# while True:
-    _, frame = video_capture.read()
-    #canvas = detect(gray, frame)
-    #image, face =face_detector(frame)
-    
-    face=face_extractor(frame)
-    if type(face) is np.ndarray:
-        face = cv2.resize(face, (224, 224))
-        im = Image.fromarray(face, 'RGB')
-           #Resizing into 128x128 because we trained the model with this image size.
-        img_array = np.array(im)
-                    #Our keras model used a 4D tensor, (images x height x width x channel)
-                    #So changing dimension 128x128x3 into 1x128x128x3 
-        img_array = np.expand_dims(img_array, axis=0)
-        pred = model.predict(img_array)
-        print(pred)
-                     
-        name="None matching"
-        
-        if(pred[0][0]>0.5):
-            # name='Subhomoy'
-            return "unlocked"
-        cv2.putText(frame,name, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
-    else:
-        cv2.putText(frame,"No face found", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
-    # cv2.imshow('Video', frame)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
-    # video_capture.release()
-    # cv2.destroyAllWindows()
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-       
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            # print(name)
+            return 'unlocked'
+        
 
