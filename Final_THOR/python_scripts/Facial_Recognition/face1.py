cc696f083f131da9e2e869abfdd2afdfa2f27cb8
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2021-03-14 12:21:11 +0530

    1

--- Final_THOR/python_scripts/Facial_Recognition/face1.py
+++ Final_THOR/python_scripts/Facial_Recognition/face1.py
@@ -29,26 +29,6 @@ def findEncodings(images):
     return encodeList
 
 
-# def markAttendance(name):
-#     with open('Attendance.csv', 'r+') as f:
-#         myDataList = f.readlines()
-
-
-#         nameList = []
-#         for line in myDataList:
-#             entry = line.split(',')
-#             nameList.append(entry[0])
-#             if name not in nameList:
-#                 now = datetime.now()
-#                 dtString = now.strftime('%H:%M:%S')
-#                 f.writelines(f'\n{name},{dtString}')
-
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-# def captureScreen(bbox=(300,300,690+300,530+300)):
-#     capScr = np.array(ImageGrab.grab(bbox))
-#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-#     return capScr
-
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
@@ -56,7 +36,6 @@ cap = cv2.VideoCapture(0)
 
 def face():
     success, img = cap.read()
-# img = captureScreen()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
@@ -66,7 +45,6 @@ def face():
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-# print(faceDis)
         matchIndex = np.argmin(faceDis)
 
         if matches[matchIndex]:
